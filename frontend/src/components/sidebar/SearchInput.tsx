6f6efc256c352dd1f56188cafe67649f3cd3d2e0
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-12-19 17:38:34 +0200

    feat: added search for chat with user

--- frontend/src/components/sidebar/SearchInput.tsx
+++ frontend/src/components/sidebar/SearchInput.tsx
@@ -1,15 +1,49 @@
-import { Search } from "lucide-react";
+import { Search } from 'lucide-react';
+import useConversation from '../../store/conversation.store';
+import { useGetConversations } from '../../hooks/conversations.hooks';
+import toast from 'react-hot-toast';
 
 const SearchInput = () => {
+	const { setSelectedConversation } = useConversation();
+	const { conversations } = useGetConversations();
+
+	const submitAction = (fd: FormData) => {
+		const search = fd.get('search');
+
+		if (typeof search !== 'string' || !search.trim()) {
+			return;
+		}
+
+		if (search.length < 3) {
+			toast.error('Search term has to be at least 3 characters long.');
+			return;
+		}
+
+		const conversation = conversations.find((c) =>
+			c.fullName.toLowerCase().includes(search.toLowerCase())
+		);
+
+		if (conversation) {
+			setSelectedConversation(conversation);
+		} else {
+			toast.error('No user is found.');
+		}
+	};
+
 	return (
-		<form className='flex items-center gap-2'>
+		<form action={submitAction} className="flex items-center gap-2">
 			<input
-				type='text'
-				placeholder='Search…'
-				className='input-sm md:input input-bordered rounded-full sm:rounded-full w-full'
+				type="text"
+				placeholder="Search…"
+				className="input-sm md:input input-bordered rounded-full sm:rounded-full w-full"
+				id="search"
+				name="search"
 			/>
-			<button type='submit' className='btn md:btn-md btn-sm btn-circle bg-sky-500 text-white  '>
-				<Search className='w-4 h-4 md:w-6 md:h-6 outline-none' />
+			<button
+				type="submit"
+				className="btn md:btn-md btn-sm btn-circle bg-sky-500 text-white  "
+			>
+				<Search className="w-4 h-4 md:w-6 md:h-6 outline-none" />
 			</button>
 		</form>
 	);
--- frontend/src/hooks/messages.hooks.ts
+++ frontend/src/hooks/messages.hooks.ts
@@ -31,7 +31,7 @@ export const useGetMessages = () => {
 		};
 
 		getMessages();
-	}, []);
+	}, [selectedConversation, setMessages]);
 
 	return { loading, messages };
 };
